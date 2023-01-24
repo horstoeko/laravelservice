6bc37f539f74a481507e2f3f31e4e7bcf77015ec
ruff <ruff@thor>
2023-01-24 17:11:59 +0100

    Added service provider

---
+++ src/LSRServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace horstoeko\laravelservicerepository;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class LSRServiceProvider
+ */
+class LSRServiceProvider extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function boot(): void
+    {
+    }
+}
