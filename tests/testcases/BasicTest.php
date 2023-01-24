decf336c9b64a2ad725269e0b11e9043621eaa9e
ruff <ruff@thor>
2023-01-24 17:14:52 +0100

    Fix Soundcheck test

--- tests/testcases/BasicTest.php
+++ tests/testcases/BasicTest.php
@@ -2,12 +2,11 @@
 
 namespace horstoeko\laravelextendedcommand\tests\testcases;
 
-use horstoeko\laravelextendedcommand\ExtendedCommand;
-use horstoeko\laravelextendedcommand\tests\TestCase;
+use horstoeko\laravelservicerepository\tests\TestCase;
 
 class BasicTest extends TestCase
 {
-    public function testClassExists(): void
+    public function testSoundCheck(): void
     {
         $this->assertTrue(true);
     }
