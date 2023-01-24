17a8830ddc939e9149be5d3ebfe5309597428012
ruff <ruff@thor>
2023-01-24 05:34:29 +0100

    Added build infrastructure

---
+++ tests/testcases/BasicTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace horstoeko\laravelextendedcommand\tests\testcases;
+
+use horstoeko\laravelextendedcommand\ExtendedCommand;
+use horstoeko\laravelextendedcommand\tests\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function testClassExists(): void
+    {
+        $this->assertTrue(true);
+    }
+}
