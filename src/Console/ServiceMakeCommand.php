2ae89f586245e57e7d7237ce78953c96aa228830
ruff <ruff@thor>
2023-01-27 06:22:09 +0100

    Fix namespaces

--- src/Console/ServiceMakeCommand.php
+++ src/Console/ServiceMakeCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace horstoeko\laravelservicerepository\console;
+namespace horstoeko\laravelservicerepository\Console;
 
 use Illuminate\Console\GeneratorCommand;
 use Symfony\Component\Console\Input\InputOption;
